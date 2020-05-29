0d4de3c3bfb823be0e80a94b424bcded7241c401
nicolas-ea <nicolas@minimo.io>
2020-05-28 21:22:14 -0300

    alt v. 0.5.5

--- functions.php
+++ functions.php
@@ -289,7 +289,7 @@ function alt_scripts() {
 	// wp_enqueue_style( 'alt-minimo-base', get_template_directory_uri()."/assets/css/minimo.css" );
 	// wp_enqueue_style( 'alt-style', get_stylesheet_uri(), Array( "alt-main-minimo", "alt-bootstrap-minimo", "alt-animate", "alt-minimo-base"  ) );
 
-	wp_enqueue_style( 'alt-style', 'https://cdn.jsdelivr.net/npm/@minimo-labs/alt-template@0.5.4/dist/alt.min.css', Array( "alt-bootstrap-minimo", "alt-animate"  ) );
+	wp_enqueue_style( 'alt-style', 'https://cdn.jsdelivr.net/npm/@minimo-labs/alt-template@0.5.5/dist/alt.min.css', Array( "alt-bootstrap-minimo", "alt-animate"  ) );
 
 
 
@@ -299,7 +299,7 @@ function alt_scripts() {
 	wp_enqueue_script( 'alt-bootstrap', get_template_directory_uri().'/assets/js/bootstrap.min.js', array( 'jquery', 'alt-popper' ), NULL, true );
 
 	// wp_enqueue_script( 'alt-script', get_template_directory_uri() . '/assets/js/functions.js', array( 'jquery' ), NULL , true );
-	wp_enqueue_script( 'alt-script', 'https://cdn.jsdelivr.net/npm/@minimo-labs/alt-template@0.5.4/dist/alt.min.js', array( 'jquery' ), NULL , true );
+	wp_enqueue_script( 'alt-script', 'https://cdn.jsdelivr.net/npm/@minimo-labs/alt-template@0.5.5/dist/alt.min.js', array( 'jquery' ), NULL , true );
 
 	if ( is_singular() ) wp_enqueue_script( "comment-reply" );
 
