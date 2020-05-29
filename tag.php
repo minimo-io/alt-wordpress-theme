d781dfaf970750f9c7df878d2a7ef7708d85dbff
nicolas-ea <nicolas@minimo.io>
2020-05-28 21:02:23 -0300

    Remove esc_html in category and tags descriptions.

--- tag.php
+++ tag.php
@@ -11,7 +11,7 @@ $is_readmore = alt_reveal_more_button($t_category_description);
     <header class="alt-post-header alt-top-title category-header white-back">
       <a class="breadcrumb-back-button" href="<?php echo esc_attr(home_url()); ?>" aria-expanded="true" role="button"><i class="fas fa-reply"></i>&nbsp;<?php _e("back", "altminimo"); ?></a>
       <h1 class="post-title basic-mono category-title" itemprop="name headline"><?php single_cat_title(); ?><span class="text-<?php echo alt_tax_color($o_cat); ?>">.</span></h1>
-      <h2 class="post-subtitle category-subtitle <?php echo (!empty($is_readmore) ? "read-more" : ""); ?>"><?php echo esc_html($t_category_description); ?></h2>
+      <h2 class="post-subtitle category-subtitle <?php echo (!empty($is_readmore) ? "read-more" : ""); ?>"><?php echo $t_category_description; ?></h2>
       <?php echo ($is_readmore ? $is_readmore : ""); ?>
     </header>
 
