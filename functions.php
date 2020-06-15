46a99c38cbe10a10da4b0fa7af72c6880ea4ce97
nicolas-ea <nicolas@minimo.io>
2020-06-15 11:57:25 -0300

    So pusher?

--- functions.php
+++ functions.php
@@ -293,6 +293,11 @@ function alt_scripts() {
 
 
 
+
+
+
+
+
 	wp_enqueue_script( 'alt-html5', get_template_directory_uri() . '/assets/js/html5.js', array(), '3.7.3' );
 	wp_script_add_data( 'alt-html5', 'conditional', 'lt IE 9' );
 	wp_enqueue_script( 'alt-popper', get_template_directory_uri() . '/assets/js/popper.min.js', array( 'jquery' ), NULL, true );
